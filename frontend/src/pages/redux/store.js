609065df3d30ba5debedcaf3094343e12fd75866
Mike Rushton <mrush94ton@gmail.com>
2024-02-08 20:06:51 +0000

    created auth slice in redux

--- frontend/src/pages/redux/store.js
+++ frontend/src/pages/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListener } from '@reduxjs/toolkit/query/react'
+import authReducer from './features/auth/authSlice.js'
 
 const store = configureStore({
     reducer: {
-
+        auth: authReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
