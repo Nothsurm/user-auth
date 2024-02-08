623036189e843ab3c5eb49317f7412a4ac36e131
Mike Rushton <mrush94ton@gmail.com>
2024-02-08 19:58:19 +0000

    created boilerplate redux store and browserrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import store from './pages/redux/store.js'
+import { Provider } from 'react-redux'
+import { Route, RouterProvider, createRoutesFromElements } from 'react-router'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+
+        </Route>
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+<Provider store={store}>
+    <RouterProvider router={router} />
+</Provider>
 )
---
+++ frontend/src/pages/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListener } from '@reduxjs/toolkit/query/react'
+
+const store = configureStore({
+    reducer: {
+
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
+})
+
+setupListener(store.dispatch)
+
+export default store;
